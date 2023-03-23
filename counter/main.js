0405ef7ab17ad91af81554b5124549ae5f56531c
bobbyy16 <bobbyyyyy16@gmail.com>
2023-03-23 14:33:48 +0530

    initial commit

---
+++ counter/main.js
@@ -0,0 +1,14 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  <button>Count: 0</button>
+  `
+
+  let count = 0
+  const button = document.querySelector('#app button')
+  button.addEventListener('click', () => {
+    count++
+    button.textContent = `Count: ${count}`
+  })
